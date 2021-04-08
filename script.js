be66d1a9a3563e709dc03e017ebc4059974af62e
Hansanghyeon <999@hyeon.pro>
2021-04-08 23:51:51 +0900

    :sparkles: 탭 활성화 스크립트

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const catControllers = document.querySelectorAll("#catController");
 
+const uxuiTab = document.querySelector("#uxui");
+const industrialTab = document.querySelector("#industrial");
+
 catControllers.forEach((catController) => {
   catController.addEventListener("click", (e) => {
     // 클래스 초기화
@@ -9,6 +12,21 @@ catControllers.forEach((catController) => {
     // // 클래스 부여
     if (!catController.classList.contains("active")) {
       catController.classList.add("active");
+    }
+    console.log(catController);
+
+    // 탭선택 액티브 부여
+    // uxui 컨트롤
+    if (catController.classList.contains("uxui")) {
+      uxuiTab.classList.add("active");
+    } else {
+      uxuiTab.classList.remove("active");
+    }
+    // industrial 컨트롤
+    if (catController.classList.contains("industrial")) {
+      industrialTab.classList.add("active");
+    } else {
+      industrialTab.classList.remove("active");
     }
   });
 });
